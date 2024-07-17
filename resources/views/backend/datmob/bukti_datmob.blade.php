a93867a74e6b42b681d0648b06a4baf9a6c8aac1
Putri <putricantip3013@gmail.com>
2024-07-17 14:41:27 +0700

    data mobil

--- resources/views/backend/datmob/bukti_datmob.blade.php
+++ resources/views/backend/datmob/bukti_datmob.blade.php
@@ -5,36 +5,32 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1 class="h3 mb-2 text-gray-800">Tambah Data Mobil</h1>
+                <h1 class="h3 mb-2 text-gray-800">Update Data Mobil</h1>
             </div>
         </div>
     </div>
     <br>
     <div class="card shadow mb-4">
         <div class="card-body">
-            <form method="POST" action="{{ route('datmob.store') }}" enctype="multipart/form-data">
+        <form method="POST" action="{{ route('bukti.update', $databukti->id) }}" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-3">
                     <div class="row">
                         <div class="col">
                             <label for="nama" class="form-label">Nama Mobil</label>
-                            <input type="text" class="form-control" name="nama" required>
+                            <input type="text" class="form-control" name="nama" value="{{$databukti->nama}}" disabled>
                         </div>
                         <div class="col">
                             <label for="merk" class="form-label">Merk</label>
-                            <input type="text" class="form-control" name="merk" required>
+                            <input type="text" class="form-control" name="merk" value="{{$databukti->merk}}" disabled>
                         </div>
                     </div>
                 </div>
                 <div class="mb-3">
                     <div class="row">
                         <div class="col">
-                            <label for="tahun_keluaran" class="form-label">Tahun Keluaran</label>
-                            <input type="text" class="form-control" name="tahun_keluaran" required>
-                        </div>
-                        <div class="col">
                             <label for="harga" class="form-label">Harga</label>
-                            <input type="text" class="form-control" name="harga" required>
+                            <input type="text" class="form-control" name="harga">
                         </div>
                     </div>
                 </div>
@@ -45,13 +41,3 @@
     </div>
 </div>
 @endsection
-
-@push('js')
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script>
-    $('.js-example-basic-multiple').select2({
-        maximumSelectionLength: 10,
-        multiple:true,
-});
-</script>
-@endpush
