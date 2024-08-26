5af95f05475140e1b3e6e2a816b1e8e02cf9bafe
nanda <nandaasari01@gmail.com>
2024-08-26 10:15:04 +0700

    excel

--- app/Exports/DattranExport.php
+++ app/Exports/DattranExport.php
@@ -5,20 +5,17 @@ namespace App\Exports;
 use App\Models\Dattran;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class DattranExport implements FromCollection, WithHeadings
+class DattranExport implements FromCollection, WithHeadings, WithMapping, WithEvents
 {
-    /**
-     * @return \Illuminate\Support\Collection
-     */
     public function collection()
     {
         return Dattran::all();
     }
 
-    /**
-     * @return array
-     */
     public function headings(): array
     {
         return [
@@ -28,6 +25,30 @@ class DattranExport implements FromCollection, WithHeadings
             'Tanggal Pinjam',
             'Tanggal Kembali',
             'Harga',
+        ];
+    }
+
+    public function map($dattran): array
+    {
+        return [
+            $dattran->id,
+            $dattran->nama,
+            $dattran->merk,
+            $dattran->tgl_pinjam,
+            $dattran->tgl_kembali,
+            $dattran->harga,
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $total = Dattran::sum('harga');
+                $event->sheet->appendRows([
+                    ['', '', '', '', 'Total Transaksi:', $total]
+                ], $event);
+            },
         ];
     }
 }
