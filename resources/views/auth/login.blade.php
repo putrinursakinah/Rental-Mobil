577d03fafa22a8f19fff50741740d8b17f5e4e3c
Putri <putricantip3013@gmail.com>
2024-08-30 08:18:16 +0700

    putri

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,42 +9,42 @@
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
-            </div>
+</div>
+@endif
+
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+
+    <div>
+        <x-label for="email" value="{{ __('Email') }}" />
+        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+    </div>
+
+    <div class="mt-4">
+        <x-label for="password" value="{{ __('Password') }}" />
+        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+    </div>
+
+    <div class="block mt-4">
+        <label for="remember_me" class="flex items-center">
+            <x-checkbox id="remember_me" name="remember" />
+            <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+        </label>
+    </div>
+
+    <div class="flex items-center justify-end mt-4">
+        @if (Route::has('password.request'))
+        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+            {{ __('Forgot your password?') }}
+        </a>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
+        <x-button class="ms-4">
+            {{ __('Log in') }}
+        </x-button>
+    </div>
+</form>
+</x-authentication-card>
 </x-guest-layout> --}}
 
 <!DOCTYPE html>
@@ -84,91 +84,94 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form class="user" method="POST" action="{{ route('login') }}">
-                                        @csrf
-
-                                        <div class="form-group">
-                                            <input type="email"  class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address..." name="email" :value="old('email')" required autofocus autocomplete="username">
-                                            
+                            <div class="col-md-6 side-image">
+                                <img src="{{asset('backend/img/kreatindo.png')}}" alt="" style="height: 450px; width: 470px;">
+                            </div>
+                    
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                         </div>
+                                        <form class="user" method="POST" action="{{ route('login') }}">
+                                            @csrf
 
-                                        {{-- <div>
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user"
+                                                    id="exampleInputEmail" aria-describedby="emailHelp"
+                                                    placeholder="Enter Email Address..." name="email" :value="old('email')" required autofocus autocomplete="username">
+
+                                            </div>
+
+                                            {{-- <div>
                                             <x-label for="email" value="{{ __('Email') }}" />
                                             <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                                        </div> --}}
+                                    </div> --}}
 
-                                        @error('email')
-                                                <small>{{ $message }}</small>
-                                            @enderror
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password" name="password" required autocomplete="current-password">
-                                        </div>
+                                    @error('email')
+                                    <small>{{ $message }}</small>
+                                    @enderror
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password" name="password" required autocomplete="current-password">
+                                    </div>
 
-                                        {{-- <div class="mt-4">
+                                    {{-- <div class="mt-4">
                                             <x-label for="password" value="{{ __('Password') }}" />
-                                            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-                                        </div> --}}
-                            
-                                        @error('password')
-                                                <small>{{ $message }}</small>
-                                            @enderror
-                                        {{-- <div class="form-group">
+                                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                                </div> --}}
+
+                                @error('password')
+                                <small>{{ $message }}</small>
+                                @enderror
+                                {{-- <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                                 <label class="custom-control-label" for="customCheck">Remember
                                                     Me</label>
                                             </div>
                                         </div> --}}
-                                        <div class="block mt-4">
-                                            <label for="remember_me" class="flex items-center">
-                                                <x-checkbox id="remember_me" name="remember" />
-                                                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                                            </label>
-                                        </div>
-                                        <x-button class="btn btn-primary btn-user btn-block">
-                                            {{ __('Log in') }}
-                                        </x-button>
-                                        {{-- <a href="{{ route('dashboard') }}" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a> --}}
-                                        {{-- <hr>
+                                <div class="block mt-4">
+                                    <label for="remember_me" class="flex items-center">
+                                        <x-checkbox id="remember_me" name="remember" />
+                                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                    </label>
+                                </div>
+                                <x-button class="btn btn-primary btn-user btn-block">
+                                    {{ __('Log in') }}
+                                </x-button>
+                                {{-- <a href="{{ route('dashboard') }}" class="btn btn-primary btn-user btn-block">
+                                Login
+                            </a> --}}
+                            {{-- <hr>
                                         <a href="index.html" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
                                         <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                         </a> --}}
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        {{-- <a class="small" href="forgot-password.html">Forgot Password?</a> --}}
-                                        @if (Route::has('password.request'))
-                                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                                                {{ __('Forgot your password?') }}
-                                            </a>
-                                        @endif
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="admin.auth.register">Create an Account!</a>
-                                    </div>
-                                </div>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                {{-- <a class="small" href="forgot-password.html">Forgot Password?</a> --}}
+                                @if (Route::has('password.request'))
+                                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                                @endif
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="admin.auth.register">Create an Account!</a>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
+
+    </div>
+
+    </div>
 
     </div>
 
