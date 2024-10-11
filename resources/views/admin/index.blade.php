5e33a0b32e762ca17d5906bb8b272c89a45772ff
Putri <putricantip3013@gmail.com>
2024-10-11 08:38:23 +0700

    putri

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,7 +15,6 @@
 
     <!-- Content Row -->
     <div class="row">
-        <form method="POST" action="{{ route('admin.index') }}" enctype="multipart/form-data">
 
         <!-- Cars (Total Stock) -->
         <div class="col-xl-3 col-md-6 mb-4">
@@ -41,7 +40,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Revenue</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. {{ $total }} </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. {{ $total }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -58,7 +57,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tenant</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"> {{ $penyewa }}</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $penyewa }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-users fa-2x text-gray-300"></i>
